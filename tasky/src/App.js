974c230e86cbc457e4829f635d8a70962b2cf772
zft666zft <2810458566@qq.com>
2024-09-27 14:05:31 +0100

    Completed Tasky Lab 1

---
+++ tasky/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Task from './components/Task';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Tasky</h1>
+      <Task title="Dishes" deadline="Today">
+      Empty dishwasher
+      </Task>
+      <Task title="Laundry" deadline="Tomorrow">
+        Fold laundry and put away
+        </Task>
+      <Task title="Tidy" deadline="Today"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ tasky/src/components/Task.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Task = (props) =>{
+
+    return(
+        <div className="card">
+        <p className='title'> {props.title} </p>
+        <p> Due: {props.deadline} </p>
+        <p className='ch'> {props.children} </p>
+        </div>
+    )
+}
+
+export default Task;
