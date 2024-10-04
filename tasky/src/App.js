648507be9c4081187fb97834f61f5da88a4fcf17
zft666zft <2810458566@qq.com>
2024-10-04 14:52:33 +0100

    Add task functionality

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Task from './components/Task';
 import React, { useState } from 'react';
 import AddTaskForm from './components/Form';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
   const [ taskState, setTaskState ] = useState({
@@ -25,6 +26,43 @@ function App() {
     tasks.splice(taskIndex, 1);
     setTaskState({tasks});
   } 
+
+  const [ formState, setFormState ] = useState({
+    title: "",
+    description: "",
+    deadline: ""
+  });
+
+  const formChangeHandler = (event) => {
+    let form = {...formState};
+
+    switch(event.target.name) {
+      case "title":
+          form.title = event.target.value;
+          break;
+      case "description":
+          form.description = event.target.value;
+          break;
+      case "deadline":
+          form.deadline = event.target.value;
+          break;
+      default:
+          form = formState;
+    }
+    setFormState(form);
+  }
+  
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    const tasks = [...taskState.tasks];
+    const form = {...formState};
+
+    form.id = uuidv4();
+    
+    tasks.push(form);
+    setTaskState({tasks});
+  }
   
   // return (
   //   <div className="container">
@@ -55,7 +93,8 @@ function App() {
       deleteTask = {() => deleteHandler(index)}
     />
   ))} 
-  <AddTaskForm />
+
+<AddTaskForm submit={formSubmitHandler} change={formChangeHandler} />
     </div>
   );
 }
