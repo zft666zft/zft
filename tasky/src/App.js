d8761cc183152dba6edfe419626c7bea9c4625f6
zft666zft <2810458566@qq.com>
2024-10-04 13:51:12 +0100

    Added keys to tasks

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -6,9 +6,9 @@ import React, { useState } from 'react';
 function App() {
   const [ taskState, setTaskState ] = useState({
     tasks: [
-      { title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
-      { title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
-      { title: "Tidy up", deadline: "Today" }
+      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
+      { id: 3, title: "Tidy up", deadline: "Today" }
     ]
   });
   // return (
@@ -34,6 +34,7 @@ function App() {
       title={task.title}
       description={task.description}
       deadline={task.deadline}
+      key={task.id}
     />
   ))} 
     </div>
