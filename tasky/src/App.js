8d2c8170e8f553ae077f89649691810266d926ad
zft666zft <2810458566@qq.com>
2024-10-04 14:28:14 +0100

    Delete a task

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -11,12 +11,19 @@ function App() {
       { id: 3, title: "Tidy up", deadline: "Today", done: false}
     ]
   });
+
   const doneHandler = (taskIndex) => {
     const tasks = [...taskState.tasks];
     tasks[taskIndex].done = !tasks[taskIndex].done;
     setTaskState({tasks});
     console.log(`${taskIndex} ${tasks[taskIndex].done}`);
   }
+
+  const deleteHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks.splice(taskIndex, 1);
+    setTaskState({tasks});
+  } 
   
   // return (
   //   <div className="container">
@@ -44,6 +51,7 @@ function App() {
       key={task.id}
       done={task.done}
       markDone={() => doneHandler(index)}
+      deleteTask = {() => deleteHandler(index)}
     />
   ))} 
 
