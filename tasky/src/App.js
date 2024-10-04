f4ff819f8a1be759d7860d1c06a8221c74b1303d
zft666zft <2810458566@qq.com>
2024-10-04 14:34:19 +0100

    Add task: form added

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Task from './components/Task';
 import React, { useState } from 'react';
+import AddTaskForm from './components/Form';
 
 function App() {
   const [ taskState, setTaskState ] = useState({
@@ -54,7 +55,7 @@ function App() {
       deleteTask = {() => deleteHandler(index)}
     />
   ))} 
-
+  <AddTaskForm />
     </div>
   );
 }
