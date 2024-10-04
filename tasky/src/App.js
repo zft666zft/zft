3dd1e424d390e616947bd067ecf56c384a60cddd
zft666zft <2810458566@qq.com>
2024-10-04 13:39:53 +0100

    Added task state

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -1,18 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 import Task from './components/Task';
+import React, { useState } from 'react';
 
 function App() {
+  const [ taskState, setTaskState ] = useState({
+    tasks: [
+      { title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
+      { title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
+      { title: "Tidy up", deadline: "Today" }
+    ]
+  });
+  // return (
+  //   <div className="container">
+  //     <h1>Tasky</h1>
+  //     <Task title="Dishes" deadline="Today" description="Empty dishwasher">
+      
+  //     </Task>
+  //     <Task title="Laundry" deadline="Tomorrow" description="Fold laundry and put away">
+        
+  //       </Task>
+  //     <Task title="Tidy" deadline="Today"/>
+  //   </div>
+  // );
   return (
     <div className="container">
       <h1>Tasky</h1>
-      <Task title="Dishes" deadline="Today" description="Empty dishwasher">
-      
-      </Task>
-      <Task title="Laundry" deadline="Tomorrow" description="Fold laundry and put away">
-        
-        </Task>
-      <Task title="Tidy" deadline="Today"/>
+      <Task title={taskState.tasks[0].title} deadline={taskState.tasks[0].deadline} description={taskState.tasks[0].description} />
+      <Task title={taskState.tasks[1].title} deadline={taskState.tasks[1].deadline} description={taskState.tasks[1].description} />
+      <Task title={taskState.tasks[2].title} deadline={taskState.tasks[2].deadline} description={taskState.tasks[2].description} />
     </div>
   );
 }
